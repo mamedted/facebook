6ed3f57eb886837a9dc09cfe0a3a28b81018be89
mamedted <mamedtedrasul20@gmail.com>
2026-01-21 19:54:23 -0800

    Added vite-svg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 import { VitePWA } from "vite-plugin-pwa";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [
     react(),
+    svgr(),
     mkcert(),
     VitePWA({
       registerType: "autoUpdate",
